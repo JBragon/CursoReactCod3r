ae99f576abaf48d0340d86fab6866653cc57dcce
Jordan Bragon <jordan10_aj@hotmail.com>
2019-04-25 20:32:37 -0300

    Aula 17

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,16 +1,23 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import PrimeiroComponente from './componentes/PrimeiroComponente'
-import {CompA, CompB} from './componentes/DoisComponentes'
+import MultiElementos from './componentes/MultiElementos'
+//import PrimeiroComponente from './componentes/PrimeiroComponente'
+//import {CompA, CompB} from './componentes/DoisComponentes'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <CompA valor="Olá, eu sou o A!"/>
-        <CompB valor="Olá, eu sou o B!"/>
+        <MultiElementos />
     </div>
 , elemento)
+
+// ReactDOM.render(
+//     <div>
+//         <CompA valor="Olá, eu sou o A!"/>
+//         <CompB valor="Olá, eu sou o B!"/>
+//     </div>
+// , elemento)
 
 // ReactDOM.render(
 //     <div>
