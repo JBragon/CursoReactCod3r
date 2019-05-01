631aeb89e5a4170490607741cf748cd851793400
Jordan Bragon <jordan10_aj@hotmail.com>
2019-05-01 19:19:00 -0300

    Aula 58

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+import todoForm from './todoForm';
 
 export default class Todo extends Component {
 
@@ -7,6 +10,8 @@ export default class Todo extends Component {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
