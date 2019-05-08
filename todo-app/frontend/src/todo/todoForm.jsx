50428005c572860eead3511a1a52568c62802367
jordan.bragon <jordan.bragon@valenet.com.br>
2019-05-08 07:59:25 -0300

    Aula 94

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -48,7 +48,7 @@ class TodoForm extends Component {
                         onClick={() => add(description)}/>
 
                     <IconButton style='info' icon='search'
-                        onClick={() => search()}/>
+                        onClick={search}/>
 
                     <IconButton style='default' icon='close'
                         onClick={() => clear()}/>
