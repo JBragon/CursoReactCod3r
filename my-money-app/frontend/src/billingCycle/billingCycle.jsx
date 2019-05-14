7908b463d134af269a2fb97093b8489938714289
Jordan Bragon <jordan10_aj@hotmail.com>
2019-05-14 20:01:40 -0300

    Aula 146

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -10,7 +10,8 @@ import TabHeader from '../common/tab/tabHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
-import List from './billingCycleList';
+import List from './billingCycleList'
+import Form from './billingCycleForm'
 
 class BillingCycles extends Component {
 
@@ -35,7 +36,9 @@ class BillingCycles extends Component {
                             <TabContent id='tabList'>
                                 <List />
                             </TabContent>
-                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form />
+                            </TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
