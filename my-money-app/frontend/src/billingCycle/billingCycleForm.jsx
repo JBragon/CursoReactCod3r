6662033e9ef435e431c27c96c657bba5b396b677
Jordan Bragon <jordan10_aj@hotmail.com>
2019-05-15 20:40:43 -0300

    Aula 164

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
@@ -16,23 +17,36 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
+
                     <Field name='name' component={labelAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome' />
+
                     <Field name='month' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Mês' cols='12 4' placeholder='Informe o mês' />
+
                     <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano' />
+
+                    <Summary credit={1000} debt={100} />
+
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} 
                         field='credits' legend='Créditos'/>
+
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} 
                         field='debts' legend='Débitos' showStatus={true}/>
+
                 </div>
                 <div className='box-footer'>
+
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}> 
                         {this.props.submitLabel}
                     </button>
+
                     <button type='button' className='btn btn-default'
-                        onClick={this.props.init}> Cancelar </button>
+                        onClick={this.props.init}> 
+                        Cancelar 
+                    </button>
+                    
                 </div>
             </form>
         )
--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -27,7 +27,7 @@ class Dashboard extends Component {
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos' />
                         <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débitos' />
-                        <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} text='Valor Consolidade' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} text='Valor Consolidado' />
                     </Row>
                 </Contet>
             </div>
